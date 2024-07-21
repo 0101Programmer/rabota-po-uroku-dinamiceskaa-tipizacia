72cad7ee206304a6c632c3c83180849683576a03
0101Programmer <baranov-v-k@yandex.ru>
2024-07-21 15:08:40 +0300

    Add files via upload

---
+++ homework3.py'.py
@@ -0,0 +1,11 @@
+name = "Владимир"
+print("Name: " + name)
+age = 21
+str_age = "Age: "
+print(str_age + str(age))
+new_age = age + 1
+str_new_age = "New age: "
+print(str_new_age + str(new_age))
+is_student = True
+str_is_student = "Is student: "
+print(str_is_student + str(is_student))
